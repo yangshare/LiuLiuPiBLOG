3ced655ebc6afed8d14d203f43f784c8c70328d9
15708440182 <717449318@qq.com>
2026-06-14 22:38:19 +0800

    feat(server): 新增 PushNotificationMapper

---
+++ liuliupi-server/src/main/java/com/liuliupi/dao/PushNotificationMapper.java
@@ -0,0 +1,16 @@
+package com.liuliupi.dao;
+
+import com.liuliupi.entity.PushNotification;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 首页弹窗推送配置表 Mapper 接口
+ * </p>
+ *
+ * @author sara
+ * @since 2026-06-14
+ */
+public interface PushNotificationMapper extends BaseMapper<PushNotification> {
+
+}
