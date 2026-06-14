870a91969dffa731753e81069247868f960be326
15708440182 <717449318@qq.com>
2026-06-14 17:21:42 +0800

    test(ui): 补充 ImageUrlInput 上传选项透传测试

--- liuliupi-ui/tests/unit/ImageUrlInput.spec.js
+++ liuliupi-ui/tests/unit/ImageUrlInput.spec.js
@@ -2,6 +2,12 @@ import { shallowMount } from '@vue/test-utils'
 import ImageUrlInput from '@/components/admin/common/ImageUrlInput.vue'
 
 describe('ImageUrlInput.vue', () => {
+  const UploadPictureStub = {
+    name: 'upload-picture',
+    props: ['prefix', 'maxSize', 'maxNumber'],
+    template: '<div />'
+  }
+
   it('renders input, thumb and upload button', () => {
     const wrapper = shallowMount(ImageUrlInput, {
       propsData: { value: 'https://example.com/bg.jpg' },
@@ -33,5 +39,30 @@ describe('ImageUrlInput.vue', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted().input).toBeTruthy()
     expect(wrapper.emitted().input[0]).toEqual(['https://example.com/uploaded.jpg'])
+  })
+
+  it('passes upload options to upload picture component', () => {
+    const wrapper = shallowMount(ImageUrlInput, {
+      propsData: { value: '' },
+      attrs: {
+        prefix: 'webAvatar',
+        maxSize: 2,
+        maxNumber: 1
+      },
+      stubs: {
+        'el-input': true,
+        'el-image': true,
+        'el-button': true,
+        'el-dialog': true,
+        'upload-picture': UploadPictureStub
+      }
+    })
+
+    const uploadPicture = wrapper.findComponent(UploadPictureStub)
+    expect(uploadPicture.props()).toMatchObject({
+      prefix: 'webAvatar',
+      maxSize: 2,
+      maxNumber: 1
+    })
   })
 })
